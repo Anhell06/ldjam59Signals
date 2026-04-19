4ffddb692f7a0f614268c85369fe5ff99f9e3808
a.kochnev <andrey.kochnev@tarkov.com>
2026-04-19 17:59:43 +0300

    Gameplay Stuff
    Gameplay Stuff

--- ldjam59Signals/Assets/Scripts/Gameplay/Enemies/RedneckEnemy.cs
+++ ldjam59Signals/Assets/Scripts/Gameplay/Enemies/RedneckEnemy.cs
@@ -1,16 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RedneckEnemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Serializable]
+    public class RedneckPatrolPoint
     {
-        
+        public Transform point;
+        public float waitTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] 
+    private List<RedneckPatrolPoint> _patrolPoints;
+    
+    [SerializeField]
+    private Billboard billboard;
+
+    [SerializeField] 
+    private NavMeshAgent agent;
+
+    [SerializeField] 
+    private float _playerSearchRadius = 10;
+    
+    [SerializeField] 
+    private float _playerLostRadius = 20;
+    public NavMeshAgent Agent => agent;
+
+    private TexturePainter _texturPainter;
+
+    private Vector3 prevPosition;
+    private bool _pursuit = false;
+
+    private void Start()
     {
-        
+        _texturPainter = Game.Instance.TexturePainter;
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(PatrolRoutine());
+    }
+
+    private int i = 0;
+    private IEnumerator PatrolRoutine()
+    {
+        while (true)
+        {
+            i = (i + 1) % _patrolPoints.Count;
+            var nextPoint = _patrolPoints[i];
+            GoToPatrolPoint();
+            while (Vector3.Distance(transform.position, nextPoint.point.position) < 2)
+            {
+                if (!TryPursuitPlayer())
+                {
+                    GoToPatrolPoint();
+                }
+                yield return null;
+            }
+            var nextTime = Time.time + nextPoint.waitTime;
+            while (Time.time < nextTime)
+            {
+                if (!TryPursuitPlayer())
+                {
+                    GoToPatrolPoint();
+                }
+                yield return null;
+            }
+        }
+    }
+
+    private bool TryPursuitPlayer()
+    {
+        if (Vector3.Distance(transform.position, Game.Instance.FirstPersonController.transform.position) > _playerLostRadius)
+        {
+            _pursuit = false;
+        }
+        if (Vector3.Distance(transform.position, Game.Instance.FirstPersonController.transform.position) < _playerSearchRadius || _pursuit)
+        {
+            _pursuit = true;
+            GoToPlayer();
+        }
+
+        return _pursuit;
+    }
+
+    private void GoToPatrolPoint()
+    {
+        var nextPoint = _patrolPoints[i];
+        Agent.SetDestination(nextPoint.point.position);
+    }
+    private void GoToPlayer()
+    {
+        Agent.SetDestination(Game.Instance.FirstPersonController.transform.position);
+    }
+
+    private void Update()
+    {
+        var position = transform.position;
+        billboard.SetMovingDirection((position - prevPosition));
+        prevPosition = position;
     }
 }
